ca846ee72b1ae1dfa324e839edd18c9a718f9dbc
Test <test@example.com>
2026-01-12 19:09:56 +0500

    Update CMS: GitHub backend and admin page redirect

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function AdminPage() {
-  return (
-    <html>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Content Manager</title>
-        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
-      </head>
-      <body>
-        {/* CMS will be injected here */}
-      </body>
-    </html>
-  );
+  // Redirect to the static admin page
+  redirect('/admin/index.html');
 }
